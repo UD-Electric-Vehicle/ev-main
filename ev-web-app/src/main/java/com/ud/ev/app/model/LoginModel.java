15eeaae81f21de6f25553dc3fe2e3bc7289deebc
nitinjhamnani <nitinjhamnani28@gmail.com>
2023-06-23 00:16:05 +0530

    Adding User Service and Login Model

---
+++ ev-web-app/src/main/java/com/ud/ev/app/model/LoginModel.java
@@ -0,0 +1,13 @@
+package com.ud.ev.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginModel {
+	private String username;
+	private String password;
+}
