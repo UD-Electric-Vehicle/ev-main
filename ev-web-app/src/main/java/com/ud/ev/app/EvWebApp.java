634de67c091469b578b7f26d7cd31d3d84e746ef
nitinjhamnani <nitinjhamnani28@gmail.com>
2023-06-23 00:09:30 +0530

    Initial Commit

---
+++ ev-web-app/src/main/java/com/ud/ev/app/EvWebApp.java
@@ -0,0 +1,13 @@
+package com.ud.ev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvWebApp.class, args);
+	}
+
+}
